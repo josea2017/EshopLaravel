c6f15fbaa3a8b622296a372ad93008488fb34689
josea2017 <josea3712@gmail.com>
2018-11-30 21:19:49 -0600

    Mostrando datos de categorias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('#') }}">
+                    E-Shop
+                </a>
+                <a style="text-decoration: none;" class="navbar-icon" href="{{ url('/categories') }}">
+                    Categories
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('categories', 'CategoryController@index');
+Route::resource('categories', 'CategoryController');
