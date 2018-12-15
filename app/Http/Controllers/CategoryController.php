8fe2e130b9d012dd0de19817b499b3927de1f5fb
josea2017 <josea3712@gmail.com>
2018-12-15 10:56:10 -0600

    verificar login y nav var

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,17 @@ use Illuminate\Support\Facades\Input;
 
 class CategoryController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
